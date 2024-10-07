a24ba73fe15245f5e828e31c95919552b1a97d47
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-10-07 14:22:48 -0700

    Changed the name of the game

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My amazing game";
+const gameName = "My (soon to be) <i> outstanding </i> game!";
+
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
+header.style.color = "red";
+
 app.append(header);
