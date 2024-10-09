86e17cf36f80cee6ad5e0ecf7a6a66c79f641ff5
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-10-08 20:43:45 -0700

    Step 2 complete. Added incrementer for rat army

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,18 @@ header.style.color = "red";
 
 app.append(header);
 
+let counter: number = 0;
+
 const button = '<button name="button">"Click to recruit me" - 🐀</button>';
 const body = document.createElement("div");
+
+function increment() {
+    counter++;
+    body.innerHTML = '<button name="button">Rats recruited: ' + counter + ' 🐀</button>';
+}
+
+
+body.addEventListener("click", increment);
 body.innerHTML = button;
 body.style.fontSize = "25px";
 
