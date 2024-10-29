3d69ef535797893a4c28b7f5152938d8306d4c73
BigWiinner <150750398+BigWiinner@users.noreply.github.com>
2024-10-29 16:55:00 -0700

    Cleaned up code after having it reviewed by Brace

--- src/main.ts
+++ src/main.ts
@@ -51,54 +51,64 @@ const availableItems: Item[] = [
   },
 ];
 
+let counter: number = 0;
+
+const countDisplay = document.createElement("div");
+
+const currentRate = document.createElement("div");
+
+let amount = 0;
+
 class Button {
   text: string;
   size: string;
   cost: number;
-  increase: number;
+  increasePrice: number;
   description: string;
   button: HTMLButtonElement;
-  item_purchased: HTMLDivElement;
-  amount_purchased: number;
-  cost_message: HTMLDivElement;
+  itemPurchased: HTMLDivElement;
+  amountPurchased: number;
+  costMessage: HTMLDivElement;
+  increaseFactor: number;
   constructor(
     text: string,
     size: string,
     cost: number,
-    increase: number,
+    increasePrice: number,
     description: string
   ) {
     this.text = text;
     this.size = size;
     this.cost = cost;
-    this.increase = increase;
+    this.increasePrice = increasePrice;
     this.description = description;
     this.button = document.createElement("button");
     this.button.setAttribute("style", "border: 3px solid pink;");
     this.button.setAttribute("title", this.description);
-    this.item_purchased = document.createElement("div");
-    this.amount_purchased = 0;
-    this.cost_message = document.createElement("div");
+    this.itemPurchased = document.createElement("div");
+    this.amountPurchased = 0;
+    this.costMessage = document.createElement("div");
+    this.increaseFactor = 1.15;
     this.handleClick = this.handleClick.bind(this);
     this.create();
     this.update();
   }
 
-  create() {
+  create(): void {
     this.button.innerHTML = this.text;
     this.button.style.fontSize = this.size;
     this.button.addEventListener("click", this.handleClick);
     app.append(this.button);
     if (this.cost !== 0) {
-      this.cost_message.innerHTML = "Purchase for " + this.cost + " drafts";
-      app.append(this.cost_message);
-      this.item_purchased.innerHTML =
-        "Amount Purchased: " + this.amount_purchased;
-      app.append(this.item_purchased);
+      this.costMessage.innerHTML = "Purchase for " + this.cost + " drafts";
+      app.append(this.costMessage);
+      this.itemPurchased.innerHTML =
+        "Amount Purchased: " + this.amountPurchased;
+      app.append(this.itemPurchased);
     }
   }
 
-  update() {
+  update(): void {
     if (counter >= this.cost) {
       this.button.disabled = false;
     } else {
@@ -107,53 +117,57 @@ class Button {
     requestAnimationFrame(this.update.bind(this));
   }
 
-  handleClick() {
+  totalDrafts(counter: number): number {
+    return Math.round(counter * 10) / 10;
+  }
+
+  recruitRat() {
+    counter++;
+    this.button.innerHTML = "🐀";
+    countDisplay.innerHTML =
+      "Rat drafts: " + this.totalDrafts(counter) + " 🪖🐀";
+  }
+
+  purchaseUpgrade() {
+    counter = Math.round((counter - this.cost) * 10) / 10;
+    countDisplay.innerHTML =
+      "Rat drafts: " + this.totalDrafts(counter) + " 🪖🐀";
+    amount += Math.round(this.increasePrice * 10) / 10;
+    this.amountPurchased++;
+    this.itemPurchased.innerHTML = "Amount Purchased: " + this.amountPurchased;
+    this.cost = Math.round(this.cost * this.increaseFactor * 10) / 10;
+    this.costMessage.innerHTML = "Purchase for " + this.cost + " drafts";
+  }
+
+  handleClick(): void {
     // This was inspired by a AI Overview suggestion made by Google when I
     // searched "how to use addeventlistener in a class object"
     if (this.cost === 0) {
-      counter++;
-      this.button.innerHTML = "🐀";
-      count_display.innerHTML =
-        "Rat drafts: " + Math.round(counter * 10) / 10 + " 🪖🐀";
+      this.recruitRat();
     } else if (counter >= this.cost) {
-      counter = Math.round((counter - this.cost) * 10) / 10;
-      count_display.innerHTML =
-        "Rat drafts: " + Math.round(counter * 10) / 10 + " 🪖🐀";
-      amount += Math.round(this.increase * 10) / 10;
-      this.amount_purchased++;
-      this.item_purchased.innerHTML =
-        "Amount Purchased: " + this.amount_purchased;
-      this.cost = Math.round(this.cost * 1.15 * 10) / 10;
-      this.cost_message.innerHTML = "Purchase for " + this.cost + " drafts";
+      this.purchaseUpgrade();
     }
   }
 }
-
-let counter: number = 0;
-
-const count_display = document.createElement("div");
-
-const current_rate = document.createElement("div");
-
-let amount = 0;
 
 // Automatic incrementer
 let startTime = performance.now();
 let endTime;
 let elapsedTime = 0;
+const oneSecond = 1000;
 function timedIncrement() {
   endTime = performance.now();
   elapsedTime += endTime - startTime;
-  if (elapsedTime >= 1000) {
+  if (elapsedTime >= oneSecond) {
     if (amount) {
       counter = Math.round((counter + amount) * 10) / 10;
     }
-    elapsedTime -= 1000;
+    elapsedTime -= oneSecond;
   }
   startTime = endTime;
-  count_display.innerHTML =
+  countDisplay.innerHTML =
     "Rat drafts: " + Math.floor(counter * 10) / 10 + " 🪖🐀";
-  current_rate.innerHTML =
+  currentRate.innerHTML =
     "Current growth rate: " + Math.round(amount * 10) / 10 + "/sec";
   requestAnimationFrame(timedIncrement);
 }
@@ -161,13 +175,13 @@ timedIncrement();
 
 new Button('"Click to recruit me" - 🐀', "35px", 0, 0, "");
 
-count_display.style.fontSize = "20px";
-app.append(count_display);
+countDisplay.style.fontSize = "20px";
+app.append(countDisplay);
 
-current_rate.innerHTML =
+currentRate.innerHTML =
   "Current growth rate: " + Math.round(amount * 10) / 10 + "/sec";
-current_rate.style.fontSize = "25px";
-app.append(current_rate);
+currentRate.style.fontSize = "25px";
+app.append(currentRate);
 
 for (const i in availableItems) {
   console.log(availableItems[i]);
